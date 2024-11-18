1f694639886604e111b21d8abb98da829c7be58e
Ayham-Kamal <ayhamk2017@gmail.com>
2024-11-18 15:16:26 -0500

    seeded the db

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,21 +1,28 @@
-import Link from 'next/link';
-import { Form } from 'app/form';
-import { redirect } from 'next/navigation';
-import { createUser, getUser } from 'app/db';
-import { SubmitButton } from 'app/submit-button';
+import Link from "next/link";
+import { Form } from "app/form";
+import { redirect } from "next/navigation";
+import { createUser, getUser } from "app/db";
+import { SubmitButton } from "app/submit-button";
+import { neon } from "@neondatabase/serverless";
 
 export default function Login() {
   async function register(formData: FormData) {
-    'use server';
-    let email = formData.get('email') as string;
-    let password = formData.get('password') as string;
+    "use server";
+    let email = formData.get("email") as string;
+    let password = formData.get("password") as string;
     let user = await getUser(email);
 
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get("email");
+    // Insert the comment from the form into the Postgres database
+    await sql("INSERT INTO userMail (email) VALUES ($1)", [comment]);
+
     if (user.length > 0) {
-      return 'User already exists'; // TODO: Handle errors with useFormStatus
+      return "User already exists"; // TODO: Handle errors with useFormStatus
     } else {
       await createUser(email, password);
-      redirect('/login');
+      redirect("/login");
     }
   }
 
@@ -31,11 +38,11 @@ export default function Login() {
         <Form action={register}>
           <SubmitButton>Sign Up</SubmitButton>
           <p className="text-center text-sm text-gray-600">
-            {'Already have an account? '}
+            {"Already have an account? "}
             <Link href="/login" className="font-semibold text-gray-800">
               Sign in
             </Link>
-            {' instead.'}
+            {" instead."}
           </p>
         </Form>
       </div>
