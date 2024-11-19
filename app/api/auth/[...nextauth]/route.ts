0f664dfa16392cfb56beb3f5ccd69604b9e978ce
Ayham-Kamal <ayhamk2017@gmail.com>
2024-11-18 23:26:01 -0500

    changed export.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,21 @@
 import NextAuth from "next-auth";
 // import Providers from "next-auth/providers";
 
-export default NextAuth({
+// export default NextAuth({
+//   providers: [],
+//   secret: process.env.NEXTAUTH_SECRET, // Use an environment variable for security
+// });
+
+// export { GET, POST } from "app/auth";
+
+//import NextAuth from "next-auth";
+// import Providers from "next-auth/providers";
+
+const authHandler = NextAuth({
   providers: [],
   secret: process.env.NEXTAUTH_SECRET, // Use an environment variable for security
 });
+
+export default authHandler;
 
 export { GET, POST } from "app/auth";
