9d73d07cecbaffbaf4b23f9b5a80f6a5d9101b2b
Mohammad Awwad <131300998+miawwad@users.noreply.github.com>
2024-12-07 16:11:09 -0500

    Revert "Designed and added edit profile functionality"

--- app/protected/testSecurity/page.tsx
+++ app/protected/testSecurity/page.tsx
@@ -1,13 +1,3 @@
-// "use client";
-import { useRouter } from "next/router";
 export default function Secure() {
-  // const router = useRouter();
-  // const { id } = router.query;
-  // console.log(`id is ${router}}`);
-  return (
-    <div>
-      <p>This is secured</p>
-      <p>id </p>
-    </div>
-  );
+  return <p>This is secured</p>;
 }
